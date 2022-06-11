3dbc8bc2cd58242e78a31269e5d986e20531a3e2
hasina821 <80751503+hasina821@users.noreply.github.com>
2022-06-11 19:52:58 +0200

    Update bookmark.controller.ts

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,5 +1,17 @@
 import { Controller } from "@nestjs/common";
+import {
 
 
 @Controller()
-export class BookMarkController{}
+export class BookMarkController{
+  constructor(private readonly auth:){}
+  
+  @get('/signin')
+  signin(){
+  }
+  
+  @get('/signup')
+  signup(){
+  }
+}
+
